5a3ef35eaf91388db104fd94ce94ec588bbb0316
Julio Cesar Gonzalez Gonzalez <jcesargon7@gmail.com>
2020-03-21 17:18:27 -0600

    creacioón de componentes

--- constructora/src/app/app.module.ts
+++ constructora/src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { CasasComponent } from './casas/casas.component';
+import { DepartamentosComponent } from './departamentos/departamentos.component';
+import { AsesoresComponent } from './asesores/asesores.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CasasComponent,
+    DepartamentosComponent,
+    AsesoresComponent
   ],
   imports: [
     BrowserModule,
